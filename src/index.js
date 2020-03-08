1cc1482ac8ab03fb157602dcfb67471371acb67e
Kayode Oluwafemi <mrlapinho@gmail.com>
2020-03-08 21:53:04 +0100

    Create seperate store file for redux store creation

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { createStore, applyMiddleware } from "redux";
+
 import { Provider } from "react-redux";
 
-import rootReducer from "./reducers";
+import store from "./store";
 
 import App from "./App";
-
-const middleware = [thunk];
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-);
 
 ReactDOM.render(
   <Provider store={store}>
