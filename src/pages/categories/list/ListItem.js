72e3be6596e45bddf075b3dcd52f7946045924f7
Kayode Oluwafemi <mrlapinho@gmail.com>
2020-03-24 21:46:05 +0100

    Add delete category functionality

--- src/pages/categories/list/ListItem.js
+++ src/pages/categories/list/ListItem.js
@@ -1,4 +1,7 @@
 import React from "react";
+import swal from "@sweetalert/with-react";
+
+import baseUrl from "../../../helpers/api";
 
 import { ReactComponent as Trash } from "../../../assets/trash.svg";
 import { ReactComponent as Edit } from "../../../assets/edit.svg";
@@ -7,6 +10,29 @@ const ListItem = ({ category, navigate }) => {
   const edit = categoryId => {
     navigate(`edit/${categoryId}`);
   };
+
+  const deleteCategory = async categoryId => {
+    const confirmDeletion = await swal({
+      title: "Hold on?",
+      text: "Are you sure you want to proceed",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true
+    });
+
+    if (confirmDeletion) {
+      try {
+        await baseUrl.delete(`/categories/${categoryId}`);
+        window.location.reload();
+        swal("Category successfully deleted", {
+          icon: "success"
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="text-md text-gray-700 px-3 py-3 mb-8 md:mb-0 border-l-4 border-blue-500 shadow-lg flex justify-between items-center">
       <p>{category.name}</p>
@@ -14,6 +40,7 @@ const ListItem = ({ category, navigate }) => {
         <Trash
           className="w-5 h-5 lg:w-4 lg:h-4 mr-3 text-gray-700 hover:text-red-500 cursor-pointer"
           title="Delete customer"
+          onClick={() => deleteCategory(category._id)}
         />
         <Edit
           className="w-5 h-5 lg:w-4 lg:h-4 text-gray-700 hover:text-blue-500 cursor-pointer"
