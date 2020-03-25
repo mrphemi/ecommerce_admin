8cbfc6df1c8304969d8fea36b307022c00ea423b
Kayode Oluwafemi <mrlapinho@gmail.com>
2020-03-25 19:00:04 +0100

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Router, navigate } from "@reach/router";
-import { useDispatch } from "react-redux";
 import decode from "jwt-decode";
 
 import setAuthToken from "./helpers/setAuthToken";
 import { setCurrentUser, logout } from "./actions/auth/authActions";
+import store from "./store";
 
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
@@ -12,29 +12,24 @@ import Dashboard from "./pages/dashboard";
 
 import "./styles/app.css";
 
-function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    // Check for token to keep user logged in
-    if (localStorage.authToken) {
-      // Set auth token header auth
-      const token = localStorage.authToken;
-      setAuthToken(token);
-      // Decode token and get user info and exp
-      const decoded = decode(token);
-      // Set user and isAuthenticated
-      dispatch(setCurrentUser(decoded));
-      // redirect to dashboard
-      navigate("/dashboard");
-      // Check for expired token
-      const currentTime = Date.now() / 1000; // to get in milliseconds
-      if (decoded.exp < currentTime) {
-        // Logout user
-        dispatch(logout(navigate));
-      }
-    }
-  }, [dispatch]);
+// Check for token to keep user logged in
+if (localStorage.authToken) {
+  // Set auth token header auth
+  const token = localStorage.authToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logout(navigate));
+  }
+}
 
+function App() {
   return (
     <div className="font-body">
       <Router>
